68acb3803e12951585b09cfb6d64df1321b3023f
Johan Duque <johand.duque@gmail.com>
2016-02-08 12:59:08 +0100

    Ionic dinners app added to repository
    	new file:   .bowerrc
    	new file:   .editorconfig
    	new file:   .gitignore
    	new file:   bower.json
    	new file:   config.xml
    	new file:   gulpfile.js
    	new file:   hooks/README.md
    	new file:   hooks/after_prepare/010_add_platform_class.js
    	new file:   ionic.project
    	new file:   package.json
    	new file:   scss/ionic.app.scss
    	new file:   www/controllers/dinners.controller.js
    	new file:   www/controllers/login.controller.js
    	new file:   www/index.html
    	new file:   www/js/app.js
    	new file:   www/js/controllers.js
    	new file:   www/js/directives.js
    	new file:   www/js/routes.js
    	new file:   www/js/services.js
    	new file:   www/lib/ionic/css/ionic.css
    	new file:   www/lib/ionic/css/ionic.min.css
    	new file:   www/lib/ionic/fonts/ionicons.eot
    	new file:   www/lib/ionic/fonts/ionicons.svg
    	new file:   www/lib/ionic/fonts/ionicons.ttf
    	new file:   www/lib/ionic/fonts/ionicons.woff
    	new file:   www/lib/ionic/js/angular-ui/angular-ui-router.js
    	new file:   www/lib/ionic/js/angular-ui/angular-ui-router.min.js
    	new file:   www/lib/ionic/js/angular/angular-animate.js
    	new file:   www/lib/ionic/js/angular/angular-animate.min.js
    	new file:   www/lib/ionic/js/angular/angular-resource.js
    	new file:   www/lib/ionic/js/angular/angular-resource.min.js
    	new file:   www/lib/ionic/js/angular/angular-sanitize.js
    	new file:   www/lib/ionic/js/angular/angular-sanitize.min.js
    	new file:   www/lib/ionic/js/angular/angular.js
    	new file:   www/lib/ionic/js/angular/angular.min.js
    	new file:   www/lib/ionic/js/ionic-angular.js
    	new file:   www/lib/ionic/js/ionic-angular.min.js
    	new file:   www/lib/ionic/js/ionic.bundle.js
    	new file:   www/lib/ionic/js/ionic.bundle.min.js
    	new file:   www/lib/ionic/js/ionic.js
    	new file:   www/lib/ionic/js/ionic.min.js
    	new file:   www/lib/ionic/scss/_action-sheet.scss
    	new file:   www/lib/ionic/scss/_animations.scss
    	new file:   www/lib/ionic/scss/_backdrop.scss
    	new file:   www/lib/ionic/scss/_badge.scss
    	new file:   www/lib/ionic/scss/_bar.scss
    	new file:   www/lib/ionic/scss/_button-bar.scss
    	new file:   www/lib/ionic/scss/_button.scss
    	new file:   www/lib/ionic/scss/_checkbox.scss
    	new file:   www/lib/ionic/scss/_form.scss
    	new file:   www/lib/ionic/scss/_grid.scss
    	new file:   www/lib/ionic/scss/_items.scss
    	new file:   www/lib/ionic/scss/_list.scss
    	new file:   www/lib/ionic/scss/_loading.scss
    	new file:   www/lib/ionic/scss/_menu.scss
    	new file:   www/lib/ionic/scss/_mixins.scss
    	new file:   www/lib/ionic/scss/_modal.scss
    	new file:   www/lib/ionic/scss/_platform.scss
    	new file:   www/lib/ionic/scss/_popover.scss
    	new file:   www/lib/ionic/scss/_popup.scss
    	new file:   www/lib/ionic/scss/_progress.scss
    	new file:   www/lib/ionic/scss/_radio.scss
    	new file:   www/lib/ionic/scss/_range.scss
    	new file:   www/lib/ionic/scss/_refresher.scss
    	new file:   www/lib/ionic/scss/_reset.scss
    	new file:   www/lib/ionic/scss/_scaffolding.scss
    	new file:   www/lib/ionic/scss/_select.scss
    	new file:   www/lib/ionic/scss/_slide-box.scss
    	new file:   www/lib/ionic/scss/_slides.scss
    	new file:   www/lib/ionic/scss/_spinner.scss
    	new file:   www/lib/ionic/scss/_tabs.scss
    	new file:   www/lib/ionic/scss/_toggle.scss
    	new file:   www/lib/ionic/scss/_transitions.scss
    	new file:   www/lib/ionic/scss/_type.scss
    	new file:   www/lib/ionic/scss/_util.scss
    	new file:   www/lib/ionic/scss/_variables.scss
    	new file:   www/lib/ionic/scss/ionic.scss
    	new file:   www/lib/ionic/scss/ionicons/_ionicons-font.scss
    	new file:   www/lib/ionic/scss/ionicons/_ionicons-icons.scss
    	new file:   www/lib/ionic/scss/ionicons/_ionicons-variables.scss
    	new file:   www/lib/ionic/scss/ionicons/ionicons.scss
    	new file:   www/lib/ionic/version.json
    	new file:   www/services/dinner.service.js
    	new file:   www/views/login.html
    	new file:   www/views/my-dinners.html

---
+++ www/controllers/dinners.controller.js
@@ -0,0 +1,13 @@
+// dinners.controller.js
+angular.module('app').controller('DinnersController', ['DinnerService', DinnersController]);
+
+function DinnersController(dinnerService) {  
+    var vm = this;
+
+    dinnerService.getMyDinners().then(
+            function (dinners) {
+                vm.dinners = dinners;
+            });
+
+    return vm;
+}
